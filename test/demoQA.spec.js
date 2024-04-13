4e3b9f2412d3e076b5febdd9f1e587b4d1d76656
Andriy Shutka <shutkaintel98@gmail.com>
2024-04-13 09:29:22 +0300

    locator

--- test/demoQA.spec.js
+++ test/demoQA.spec.js
@@ -4,8 +4,9 @@ test('review', async ({page}) => {
 await page.goto('https://demoqa.com/automation-practice-form')
 // await page.locator('.card:nth-child(2)').click()
 
-await page.locator('#firstName').fill('Andriy')
-await expect(page.locator('#firstName')).toHaveValue('Andriy')
+const nameFirst = await page.getByPlaceholder('First Name')
+await nameFirst.fill('Andriy')
+await expect(nameFirst).toHaveValue('Andriy')
 
 
 
