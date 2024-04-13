cb51ef8d42fbfbf17ce9bbbfe6ed4ddedb9ac72f
Andriy Shutka <shutkaintel98@gmail.com>
2024-04-13 13:04:03 +0300

    sign up form

--- test/demoQA.spec.js
+++ test/demoQA.spec.js
@@ -1,14 +1,23 @@
 import {test, expect} from '@playwright/test'
 
+test.describe('sign up form', () =>{ 
+
 test('review', async ({page}) => {
 await page.goto('https://demoqa.com/automation-practice-form')
 // await page.locator('.card:nth-child(2)').click()
 
-const nameFirst = await page.getByPlaceholder('First Name')
-await nameFirst.fill('Andriy')
-await expect(nameFirst).toHaveValue('Andriy')
-
-
+const firstNameValue = await page.getByPlaceholder('First Name')
+await firstNameValue.fill('Andriy')
+expect(firstNameValue).toHaveValue('Andriy')
+// await expect(firstNameValue).toHaveValue('Andriy')
+})
+test('email field', async({page}) => {
+    await page.goto('https://demoqa.com/automation-practice-form')
+    const lastNameField = page.getByRole('textbox', {name: 'Last Name'})
+    await lastNameField.fill('shutkaintel98@gmail.com')
+    await page.pause(3000)
+    expect(lastNameField).toHaveValue('shutkaintel98@gmail.com')
+})
 
 
 })
