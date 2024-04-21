e23bb5cccb4eac57be686b056162197d6ccf71fe
Andriy Shutka <shutkaintel98@gmail.com>
2024-04-21 12:22:54 +0300

    before

---
+++ test/searchBox.spec.js
@@ -0,0 +1,15 @@
+import {test, expect} from '@playwright/test';
+ 
+test.describe('Search field functionality', () =>{
+    test.beforeEach('Open the main page', async({page})=>{
+         await page.goto('https://magento.softwaretestingboard.com/')
+
+test('Search box  availability on all pages'), async({page})=>{
+    await page.locator('')
+    
+}
+
+
+
+    })
+})
